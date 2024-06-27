3f3439c9f772742216b11d6a7b86a705aedc966e
Planck-Ho <wenloong.he@gmail.com>
2024-06-27 17:15:41 +0800

    feat: external @element-plus/icons-vue

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue', 'element-plus'],
+      external: ['vue', 'element-plus', '@element-plus/icons-vue'],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
